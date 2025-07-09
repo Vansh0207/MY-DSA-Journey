ea4cbd836aef2a46d7bb54376e9af452e422dd3f
Vansh Kaushik <vanshkaushik749@gmail.com>
2025-07-09 22:14:51 +0530

    2Done

--- SlidingWindow/TwoPointer.cpp/BinarySubarrayWithSum.cpp
+++ SlidingWindow/TwoPointer.cpp/BinarySubarrayWithSum.cpp
@@ -1,11 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// main logic is as following:
+//  find the number of subarrays with sum at most equal to goal
+// then find number of subarrays with sum at most equal to goal-1
+// subtract goal(2) with goal(1) to get valid and optimized answer
+
 int countBinarySubarrayWithSum(vector<int> &nums, int goal)
 {
     int l = 0, r = 0;
     int sum = 0, cnt = 0;
 
+    // to check if (goal-1 is < 0)
     if (goal < 0)
         return 0;
 
